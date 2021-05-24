1b4234dbb1077bc91b63c93b0aa9a747ec258540
Anuj Rao <anuj.rao@myntra.com>
2021-05-24 11:20:50 +0530

    some more theoritical info about useEffect hook and added one TODO also

--- src/App.js
+++ src/App.js
@@ -41,5 +41,20 @@ export default App
 // useEffect
 /**
 * useEffect function will run each time the state count or message is changed. if we want it to run only
-* in the first render then pass [] array as the depedency array
+* in the first render then pass [] array as the depedency array. (because this array ever changed. so obviously it will run for only one time)
+* Think of the useEffect hook as a partial replacement for React lifecycle events.
+* With the useEffect hook, it is possible to replicate behavior for componentDidMount, componentDidUpdate and componentWillUnmount methods.
+  Scenarios for the usage of useEffect :-
+    1. Add an event listener for a button
+    2. Fetch data from API when component mounts
+    3. Perform an action when state or props change
+    4. Clean up event listeners when the component unmounts
+  In short, the above-mentioned lifecycle methods can be replaced with the useEffect hook.
+  NOTE: The passed fuction as argument will run after every render but this depends upon the value of the dependecy array argument.
+        useEffect hook checks this dependency array to compare the previous and current value for those dependencies
+        and then it runs the callback function but only if one of the dependencies has changed.
+  
+  NOTE: The JS logic inside useEffect callback function can be moved within the function passed to the useEffect hook. We will explore the benefits of doing so.
+        TODO: document what are the disadvatages of not doing so with examples.
+
 */
