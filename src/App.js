379f4dc97d096b53535a0d948852ff22936be72d
Anuj Rao <anuj.rao@myntra.com>
2021-05-24 10:33:06 +0530

    about useEffect hook and little theory about it's use like dependency array.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    if (!message && count > 5) {
+      setMessage('Wow... you are on a click spree!')
+    }
+  }, [count, message])
+
+  const resetPressed = () => {
+    setCount(0)
+    setMessage('')
+  }
+
   return (
     <div className="App">
       <p>Current count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={resetPressed}>Reset</button>
+      {message}
     </div>
   )
 }
 
 export default App
 
+// useState
 /**
  * 
  * The useState hook is an alternative to using this.state or this.setState in class components.
@@ -21,3 +37,9 @@ export default App
  * argument.
  * 
  */
+
+// useEffect
+/**
+* useEffect function will run each time the state count or message is changed. if we want it to run only
+* in the first render then pass [] array as the depedency array
+*/
